0172157033dc8e422489e56ab2a7119c7c6fa9e9
YousifA <yosif.ph2003@gmail.com>
2025-02-16 19:31:15 +0300

    Sync Previous migrations with main

---
+++ DataLoggerDatabase/Migrations/20250106084440_StationType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLoggerDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class StationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StationType",
+                table: "Stations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StationType",
+                table: "Stations");
+        }
+    }
+}
---
+++ DataLoggerDatabase/Migrations/20250113091442_TankCurrentVol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLoggerDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class TankCurrentVol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "TankCurrentVol",
+                table: "PipesData",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TankCurrentVol",
+                table: "PipesData");
+        }
+    }
+}
